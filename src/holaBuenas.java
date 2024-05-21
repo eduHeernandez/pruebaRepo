448368544cbac12c6534e2bf7c445d8d0a50c79e
eduar <eduar@DESKTOP-3H7DHP7>
2024-05-21 10:57:45 +0200

    prueba

---
+++ src/holaBuenas.java
@@ -0,0 +1,10 @@
+
+public class holaBuenas {
+
+	public static void main(String[] args) {
+		System.out.println("SI ME DAS A ELEGIR ENTRE TU Y LA RIQUEZA, CON ESA GRANDEZA, QUE LLEVA CONSIGO, JHAY AMOR, ME QUE");
+
+
+	}
+
+}
